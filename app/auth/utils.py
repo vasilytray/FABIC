e959f8b0c9d7fabcfafb0c585bdd39a09bbc5e7c
vasilytry <vsankov@gmail.com>
2025-01-31 00:01:43 +0700

    add new

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -10,7 +10,7 @@ def create_tokens(data: dict) -> dict:
     now = datetime.now(timezone.utc)
 
     # AccessToken - 30 минут
-    access_expire = now + timedelta(seconds=10)
+    access_expire = now + timedelta(minutes=30)
     access_payload = data.copy()
     access_payload.update({"exp": int(access_expire.timestamp()), "type": "access"})
     access_token = jwt.encode(
